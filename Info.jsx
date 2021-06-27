27f32225b72ab95467dc5d1c9ce66e95bed273b5
SreeramDivyaSri <86348521+DIVYASRI1881@users.noreply.github.com>
2021-06-27 10:28:21 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+    <h1>JavaScript and React.js</h1>
+    <p>This was an amazing bootcamp taken by Shaurya Sinha Sir. We covered Everything from Scratch including JavaScript,ReactJs,HTML. MoreOver I really loved Sir teaching which has created me a great intrest in web dev.Hope I get same faculty for the other boot camp and the paid course of web dev.Thanku for creating a good intrest on webdev in us Sir.Thankyou for moulding me into my better version.THANKS A LOT Shaurya SIR</p>
+    </div>
+  );
+  }
+export default Info;
